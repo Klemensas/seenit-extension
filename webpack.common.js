57c5e45d82a078e50bbcb69b74411506d8830c70
Klemensas <hello@klemensas.net>
2019-05-27 15:00:02 +0300

    chore: add apollo deps and graphql loader

--- webpack.common.js
+++ webpack.common.js
@@ -8,39 +8,48 @@ module.exports = {
     ])
   ],
   entry: {
-    popup: path.join(__dirname, "src/popup/index.tsx"),
-    eventPage: path.join(__dirname, "src/eventPage.ts"),
-    content: path.join(__dirname, "src/content.ts"),
+    popup: path.join(__dirname, 'src/popup/index.tsx'),
+    eventPage: path.join(__dirname, 'src/eventPage.ts'),
+    content: path.join(__dirname, 'src/content/index.tsx'),
   },
   output: {
-    path: path.join(__dirname, "dist/js"),
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist/js'),
+    filename: '[name].js'
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: 'ts-loader'
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader" // Creates style nodes from JS strings
+            loader: 'style-loader' // Creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // Translates CSS into CommonJS
+            loader: 'css-loader' // Translates CSS into CommonJS
           },
           {
-            loader: "sass-loader" // Compiles Sass to CSS
+            loader: 'sass-loader' // Compiles Sass to CSS
           }
         ]
-      }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        exclude: /node_modules/,
+        loader: "graphql-tag/loader",
+        test: /\.(graphql|gql)$/,
+      },
     ]
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js']
   },
 };
