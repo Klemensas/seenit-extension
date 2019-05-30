15e8a5997a68652f738fab306a4bc0bc7a83f23a
Klemensas <hello@klemensas.net>
2019-05-30 15:25:40 +0300

    feat: add tv options to watched form

--- src/watched/WatchedForm.tsx
+++ src/watched/WatchedForm.tsx
@@ -2,40 +2,74 @@ import { Formik } from 'formik';
 import * as React from 'react';
 import Flatpickr from 'react-flatpickr';
 import Rating from 'react-rating';
+import Select, { components } from 'react-select';
 
 import 'flatpickr/dist/themes/material_green.css';
 import { Title } from '../content/renderService';
-import { TmdbMovie, TmdbTv, useAddWatchedMutation } from '../graphql';
+import { TmdbMovie, TmdbTv, useAddWatchedMutation, Season, TmdbMediaType } from '../graphql';
+
+const SingleValue = ({ children, ...props }: React.PropsWithChildren<any>) => {
+  const { data } = props;
+  return (
+    <components.SingleValue {...props}>
+      {children}
+      <span style={{ opacity: 0.5, paddingLeft: '0.5em', fontWeight: 300 }}>({data.seasonName})</span>
+    </components.SingleValue>
+  );
+};
 
 export default function WatchedForm({
   item,
-}: // title,
-{
+  title,
+  seasons,
+}: {
   item: TmdbMovie | TmdbTv;
   title?: Title;
+  seasons?: Season[];
 }): React.ReactElement {
   const tmdbId = item.id;
   const mediaType = item.media_type;
-  // TODO: figure out how to make TS happy with dot notation
-  // const name: string = item['name'] || item['title'];
   const name = (item as TmdbTv).name || (item as TmdbMovie).title;
-
   const addWatched = useAddWatchedMutation();
+  const seasonOptions = (seasons || []).map(({ season_number: season, episode_count: episodeCount, name: label }) => ({
+    label,
+    season,
+    options: Array.from({ length: episodeCount }).map((val, i) => ({
+      label: `Episode ${i + 1}`,
+      value: { season, episode: i + 1 },
+      seasonName: label,
+    })),
+  }));
+
+  let defaultTvData = null;
+  if (title && title.season) {
+    const group = seasonOptions.find(({ season }) => +title.season === season);
+    defaultTvData = group ? group.options.find(({ value }) => value.episode === +title.episode) : null;
+  }
+  const tvData = title
+    ? {
+        season: title.season,
+        episode: title.episode,
+      }
+    : null;
+
   return (
     <React.Fragment>
       <div style={{ display: 'flex' }}>
         {item.poster_path ? (
           <div>
-            <img
-              src={`https://image.tmdb.org/t/p/w92${item.poster_path}`}
-              alt=""
-            />
+            <img src={`https://image.tmdb.org/t/p/w92${item.poster_path}`} alt="" />
           </div>
         ) : null}
         <div style={{ padding: '0 0.5em' }}>Did you enjoy watching {name}?</div>
       </div>
       <Formik
-        initialValues={{ review: '', rating: null, createdAt: Date.now() }}
+        initialValues={{
+          review: '',
+          rating: null,
+          createdAt: Date.now(),
+          tvData,
+        }}
         enableReinitialize
         onSubmit={(values, actions) => {
           addWatched({
@@ -53,15 +87,7 @@ export default function WatchedForm({
           }).then(() => actions.setSubmitting(false));
         }}
       >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          setFieldValue,
-        }) => (
+        {({ values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue }) => (
           <form onSubmit={handleSubmit}>
             <label htmlFor="createdAt">
               <span>Date</span>
@@ -76,6 +102,18 @@ export default function WatchedForm({
                 }}
               />
             </label>
+            {mediaType === TmdbMediaType.Tv ? (
+              <label htmlFor="tvData">
+                <span>Episode</span>
+                <Select
+                  id="tvData"
+                  defaultValue={defaultTvData}
+                  options={seasonOptions}
+                  onChange={handleChange}
+                  components={{ SingleValue }}
+                />
+              </label>
+            ) : null}
             <label htmlFor="review">
               <span>Review</span>
               <textarea
@@ -84,23 +122,13 @@ export default function WatchedForm({
                 value={values.review}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                className={
-                  errors.review && touched.review
-                    ? 'text-input error'
-                    : 'text-input'
-                }
+                className={errors.review && touched.review ? 'text-input error' : 'text-input'}
               />
             </label>
-            {errors.review && touched.review && (
-              <div className="input-feedback">{errors.review}</div>
-            )}
+            {errors.review && touched.review && <div className="input-feedback">{errors.review}</div>}
             <label htmlFor="rating">
               Rating
-              <Rating
-                initialRating={values.rating}
-                fractions={2}
-                onChange={value => setFieldValue('rating', value)}
-              />
+              <Rating initialRating={values.rating} fractions={2} onChange={value => setFieldValue('rating', value)} />
             </label>
             <button type="submit">Add</button>
           </form>
