4fdc027e50eea4d5b4c269d6247c2faffcecc91e
Klemensas <hello@klemensas.net>
2019-05-30 15:27:02 +0300

    feat: refactor search and add tv data

--- src/watched/Search.tsx
+++ src/watched/Search.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import { useApolloClient } from 'react-apollo-hooks';
-import AsyncSelect from 'react-select/async';
-import ApolloClient from 'apollo-client';
+import Select from 'react-select';
 
-import {
-  SearchContentDocument,
-  SearchContentQuery,
-  TmdbMovie,
-  TmdbTv,
-} from '../graphql';
+import { TmdbMovie, TmdbTv, TmdbMediaType, useTvQuery, useSearchContentQuery } from '../graphql';
 import WatchedForm from './WatchedForm';
 
 export interface SearchOption {
@@ -16,71 +9,57 @@ export interface SearchOption {
   value: string | number;
 }
 
-async function searchQuery(state, setState, client: ApolloClient<unknown>) {
-  const loadState = {
-    ...state,
-    data: [],
-    error: null,
-    loading: true,
-  };
-  setState(loadState);
-  const { data, errors } = await client.query<SearchContentQuery>({
-    query: SearchContentDocument,
-    variables: { title: state.query },
-  });
-  if (errors) {
-    setState({ ...loadState, loading: false, error: errors });
-    return [];
-  }
-  const options = data.searchContent.results.reduce(
-    (acc: SearchOption[], item) =>
-      item.media_type === 'person'
-        ? acc
-        : acc.concat({
-            label: `${(item as TmdbTv).name || (item as TmdbMovie).title} (${
-              (
-                (item as TmdbMovie).release_date ||
-                (item as TmdbTv).first_air_date ||
-                ''
-              ).split('-')[0]
-            })`,
-            value: item.id,
-            ...item,
-          }),
-    [],
-  );
-  setState({ ...loadState, loading: false, data: options });
-  return options;
-}
-
 export default function Search(): React.ReactElement {
-  const [searchState, setSearchState] = React.useState({
-    data: [],
-    error: null,
-    loading: false,
-    query: '',
+  const [query, setQuery] = React.useState('');
+  const [selectedItem, setSelectedItem] = React.useState<TmdbMovie | TmdbTv>(null);
+
+  const searchQuery = useSearchContentQuery({
+    variables: { title: query },
+    skip: !query,
   });
-  const [selectedItem, setSelectedItem] = React.useState(null);
-  const client = useApolloClient();
+  const options =
+    searchQuery.data && searchQuery.data.searchContent
+      ? searchQuery.data.searchContent.results.reduce(
+          (acc: SearchOption[], item) =>
+            item.media_type === 'person'
+              ? acc
+              : acc.concat({
+                  label: `${(item as TmdbTv).name || (item as TmdbMovie).title} (${
+                    ((item as TmdbMovie).release_date || (item as TmdbTv).first_air_date || '').split('-')[0]
+                  })`,
+                  value: item.id,
+                  ...item,
+                }),
+          [],
+        )
+      : [];
+
+  const tvQuery = useTvQuery({
+    variables: { tmdbId: selectedItem ? selectedItem.id : null },
+    skip: !selectedItem || selectedItem.media_type !== TmdbMediaType.Tv,
+  });
+  const seasons = tvQuery.data && tvQuery.data.tv ? tvQuery.data.tv.seasons : null;
 
   return (
     <React.Fragment>
-      <AsyncSelect
+      <Select
         cacheOptions
-        defaultOptions={searchState.data}
-        inputValue={searchState.query}
-        noOptionsMessage={() =>
-          searchState.query ? 'No options' : 'Enter a query to search'
-        }
-        loadOptions={() => searchQuery(searchState, setSearchState, client)}
-        onChange={query => setSelectedItem(query)}
-        onInputChange={(query, { action }) => {
-          if (action === 'input-change') {
-            setSearchState({ ...searchState, query });
+        inputValue={query}
+        isLoading={searchQuery.loading}
+        options={options}
+        noOptionsMessage={() => (query ? 'No options' : 'Enter a query to search')}
+        onChange={value => setSelectedItem(value)}
+        onInputChange={(value, { action }) => {
+          if (action === 'input-change' || action === 'set-value') {
+            setQuery(value);
           }
         }}
       />
-      {selectedItem ? <WatchedForm item={selectedItem} /> : ''}
+      {selectedItem && !tvQuery.loading ? (
+        <WatchedForm item={selectedItem} seasons={selectedItem.media_type === TmdbMediaType.Tv ? seasons : null} />
+      ) : (
+        ''
+      )}
     </React.Fragment>
   );
 }
