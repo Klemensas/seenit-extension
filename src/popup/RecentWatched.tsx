b11e67c17afe324f8171ae0081b54ec152ce2f6b
Klemensas <hello@klemensas.net>
2019-10-29 23:36:38 +0200

    refactor: recent watched

--- src/popup/RecentWatched.tsx
+++ src/popup/RecentWatched.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
-import Search from '../watched/Search';
+import { Text } from '@blueprintjs/core';
 
-export default function RecentWatched() {
+import { SearchItem } from '../graphql';
+import { TmdbMediaType } from '../graphql/react';
+import Search from '../watched/Search';
+import WatchedMovieForm from '../watched/WatchedMovieForm';
+import WatchedTvForm from '../watched/WatchedTvForm';
+
+const RecentWatched = () => {
+  const [selected, setSelected] = React.useState<SearchItem>(null);
+
+  if (!selected) {
+    return (
+      <React.Fragment>
+        <Text tagName="p">Watched anything recently?</Text>
+        <Search selected={selected} setSelected={setSelected} />
+      </React.Fragment>
+    )
+  }
+
+  const WatchedForm = selected.type === TmdbMediaType.Movie ? WatchedMovieForm : WatchedTvForm
+
   return (
-    <React.Fragment>
-      <p>Watched anything recently?</p>
-      <Search />
-      {/* <div
-        style={{
-          border: '3px solid red',
-          backgroundColor: 'white',
-          padding: '.5em',
-          margin: '1em 0',
-          fontSize: '0.75em',
-        }}
-      >
-        Missing popup after finishing a video?
-      </div> */}
-    </React.Fragment>
+    <WatchedForm id={selected.id} />
   );
-}
+};
+
+export default RecentWatched;
