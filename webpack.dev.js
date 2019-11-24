c4f2b3e83496053bde12e0395f6428b3fb88817d
Klemensas <hello@klemensas.net>
2019-11-24 13:12:23 +0200

    feat: add dev icon

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,16 @@
+const CopyPlugin = require('copy-webpack-plugin');
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "inline-source-map"
+  devtool: "inline-source-map",
+  plugins: [
+    new CopyPlugin([
+      { from: 'public/', to: '../' },
+      { from: 'dev/icon.png', to: '../icon16.png', force: true },
+      { from: 'dev/icon.png', to: '../icon48.png', force: true },
+      { from: 'dev/icon.png', to: '../icon128.png', force: true },
+    ]),
+  ],
 });
