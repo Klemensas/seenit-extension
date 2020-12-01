635bfee9a5b492b50edc0c31e01c81615161f3f2
Klemensas <hello@klemensas.net>
2020-12-02 00:04:42 +0200

    Add initial auto tracked toaster (#167)

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 import { getStorageValue, updateStorage } from './common/storage';
-import { Settings as UserSettings, UserDataQuery, MeDocument, MeQuery } from './graphql';
+import { UserDataQuery, MeDocument, MeQuery } from './graphql';
 import { apolloClient } from './apollo';
+import { isProduction } from './utils/helpers';
 
-export interface Settings extends Pick<UserSettings, 'general' | 'extension'> {
-  debug: boolean;
-}
-
-export const settings: Settings = {
-  debug: true,
-  general: {
-    autoConvert: false,
-  },
-  extension: {
-    autoTrack: false,
-    minLengthSeconds: 360,
-    blacklist: [],
-  },
-};
-
-// let lastUserSync = 0;
 const syncInterval = 300000; // 5mins
 
 async function getUserData() {
@@ -29,41 +13,28 @@ async function getUserData() {
 
   return data.me;
 }
-export const settingPromise = getStorageValue<{ user: NonNullable<UserDataQuery['userData']>; lastUserSync: number }>(
-  'user',
-  'lastUserSync',
-).then(({ user, lastUserSync: lastSync }) => {
-  // lastUserSync = lastSync;
-  return Object.assign(settings, user.settings);
-});
 
 export const getSettings = async () => {
   const { user, lastUserSync } = await getStorageValue<{
-    user: NonNullable<UserDataQuery['userData']>;
+    user: UserDataQuery['userData'] | null;
     lastUserSync: number;
   }>('user', 'lastUserSync');
 
   const nextSync = lastUserSync + syncInterval - Date.now();
-  if (nextSync < 0) return user.settings;
+  if (nextSync > 0) return user?.settings;
 
   const newUser = await getUserData();
   const syncTime = Date.now();
 
-  updateStorage({ user: newUser, lastUserSync: syncTime });
+  await updateStorage({ user: newUser, lastUserSync: syncTime });
 
   return newUser.settings;
 };
 
-export function debugLog(...data) {
-  if (settings.debug) {
-    return;
-  }
+export async function debugLog(...data) {
+  // TODO: ADd debugging setting and check setttings. Is it really effective to fetch from storage every time?
+  if (isProduction) return;
 
   // eslint-disable-next-line no-console
   console.log(data);
 }
-
-browserService.addListener(({ newValue }) => {
-  Object.assign(settings, newValue);
-  debugLog('update settings', newValue, settings);
-});
