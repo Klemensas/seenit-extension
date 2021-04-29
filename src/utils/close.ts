6938b6f25069ed2296088d496767d8e0a5d1077c
Klemensas <hello@klemensas.net>
2021-04-30 01:55:50 +0300

    feat: centralize helpers

--- src/utils/close.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export const closeContent = () => {
-  const container = document.getElementById('seenit-container');
-  container?.parentNode?.removeChild(container);
-};
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,47 @@
 /* eslint-disable import/prefer-default-export */
 
 export const isProduction = process.env.NODE_ENV === 'production';
+
+let container: null | HTMLElement = null;
+
+export const getContainer = () => {
+  if (!container) throw new Error('Container not set');
+
+  return container;
+};
+
+export const setContainer = (newContainer: HTMLElement) => {
+  container = newContainer;
+};
+
+export const closeContent = () => {
+  container?.parentNode?.removeChild(container);
+};
+
+export const throttle = <T extends (...args: any) => any>(callback: T, time: number) => {
+  let isWaiting = false;
+
+  return (...args: Parameters<T>) => {
+    if (isWaiting) return;
+
+    isWaiting = true;
+    callback.apply(this, args);
+    setTimeout(() => {
+      isWaiting = false;
+    }, time);
+  };
+};
+
+export const getTokenPayload = (token?: string | null) => {
+  if (typeof token !== 'string') return null;
+
+  try {
+    const tokenPayload = token.split('.')[1];
+    const decodedToken = atob(tokenPayload);
+    const parsedToken = JSON.parse(decodedToken);
+
+    return parsedToken;
+  } catch {
+    return null;
+  }
+};
