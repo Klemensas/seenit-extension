c7543254a39ddca37c0e9a23457f7c948ca79427
Klemensas <hello@klemensas.net>
2021-04-30 01:54:33 +0300

    feat: add more controls to ProgressBar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,22 +3,34 @@ import { animated, useSpring } from 'react-spring';
 
 interface Props {
   duration: number;
+  pause?: boolean;
+  intent?: 'success' | 'warning' | 'danger' | 'primary';
   onStart?: () => void;
   onRest: () => void;
+  className?: string;
 }
 
-const ProgressBar = ({ duration, onStart, onRest }: Props) => {
-  const { value } = useSpring({ value: 0, from: { value: 100 }, config: { duration }, onRest, onStart });
+const ProgressBar = ({ duration, intent = 'primary', pause, className, onStart, onRest }: Props) => {
+  const [reset, setReset] = React.useState(false);
+  React.useEffect(() => {
+    if (!pause) setReset(true);
+  }, [pause]);
+
+  const { value } = useSpring({
+    pause,
+    value: 0,
+    from: { value: 100 },
+    config: { duration },
+    reset,
+    onRest: ({ finished }) => finished && onRest(),
+    // onStart: () => setReset(false) && onStart?.(),
+  });
 
   return (
-    <div className="bp3-progress-bar bp3-progress-bar--custom bp3-intent-success bp3-no-stripes">
-      <animated.div className="bp3-progress-meter" style={{ width: value.interpolate((v) => `${v}%`) }} />
+    <div className={`bp3-progress-bar bp3-progress-bar--custom bp3-intent-${intent} bp3-no-stripes ${className}`}>
+      <animated.div className="bp3-progress-meter" style={{ width: value.to((v) => `${v}%`) }} />
     </div>
   );
-};
-
-ProgressBar.defaultProps = {
-  onStart: () => {},
 };
 
 export default ProgressBar;
