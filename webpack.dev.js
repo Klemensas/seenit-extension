98ea33420cff72eebb25c623248fdbe38be41899
Klemensas <hello@klemensas.net>
2021-07-19 21:10:48 +0300

    Upgrade deps (#243)
    * feat: add ignoring prompt
    
    * feat: upgrade deps

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,13 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   plugins: [
-    new CopyPlugin([
+    new CopyPlugin({ patterns: [
       { from: 'public/', to: '../' },
       { from: 'dev/icon.png', to: '../icon16.png', force: true },
       { from: 'dev/icon.png', to: '../icon48.png', force: true },
@@ -16,7 +16,7 @@ module.exports = merge(common, {
       { from: 'dev/icon-inactive.png', to: '../icon48-inactive.png', force: true },
       { from: 'dev/icon-inactive.png', to: '../icon128-inactive.png', force: true },
       { from: 'dev/manifest.json', to: '../manifest.json', force: true },
-    ]),
+    ]}),
     new Dotenv(),
   ],
 });
